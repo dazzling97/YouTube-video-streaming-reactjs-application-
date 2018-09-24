6d2a5b07b91a805edfe28f712c49dfcc6939a68f
dazzling97 <sanoj.coreunit@gmail.com>
2018-09-24 21:32:13 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Youtube video Live streaming App
         </p>
+        
       </div>
     );
   }
