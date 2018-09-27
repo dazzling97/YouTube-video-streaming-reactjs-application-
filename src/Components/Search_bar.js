8160bb498dc98b13bcb868b8e4fca770cf64b5c4
dazzling97 <sanoj.coreunit@gmail.com>
2018-09-27 14:56:51 +0530

    components half

---
+++ src/Components/Search_bar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {term: ''} ;
+    }
+
+
+    render() {
+
+        return(
+                <div>
+                   
+            <input 
+             value = {this.state.term} 
+            onChange={event => this.setState({term:event.target.value}) } />
+
+            
+                </div>
+        );
+    }
+
+    
+
+
+}
+
+
+
+export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './Components/Search_bar'; 
+
+const API_KEY ='AIzaSyDL8tKOY0dYj1GWVuIqrVoj7wjaciZjRPY';
+
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
